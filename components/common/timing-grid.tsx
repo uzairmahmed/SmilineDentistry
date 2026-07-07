f2971a3f16270c9def13636f14140e5aab874ba3
Uzair Ahmed <uzairmahmed@gmail.com>
2026-07-06 20:44:00 -0400

    update clinic hours

--- components/common/timing-grid.tsx
+++ components/common/timing-grid.tsx
@@ -21,9 +21,9 @@ const TimingRow: React.FC<TimingRowProps> = ({ day, times }) => {
 const TimingGrid = () => {
     return (
         <div className='flex flex-col items-center gap-5 mt-10 text-center rounded-3xl'>
-            <TimingRow day='Sunday' times='11:00AM - 4:00PM' />
+            <TimingRow day='Sunday' times='CLOSED' />
             <TimingRow day='Monday' times='10:00AM - 7:00PM' />
-            <TimingRow day='Tuesday' times='CLOSED' />
+            <TimingRow day='Tuesday' times='12:00PM - 5:00PM' />
             <TimingRow day='Wednesday' times='10:00AM - 7:00PM' />
             <TimingRow day='Thursday' times='10:00AM - 7:00PM' />
             <TimingRow day='Friday' times='CLOSED' />
